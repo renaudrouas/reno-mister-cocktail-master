34b19890f61cf5ea1c543d20ae6e58bee109d2ab
renaud rouas <renaud.rouas@gmail.com>
2018-05-11 12:55:46 +0200

    back ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require 'faker'
+require 'open-uri'
+
+
+Ingredient.destroy_all
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_list = JSON.parse(open(url).read)["drinks"]
+
+ingredients_list.each do |ingredient_hash|
+  Ingredient.create!(
+      name: ingredient_hash["strIngredient1"]
+    )
+end
+
